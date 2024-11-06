c1e76e8f9b782c88ca36c5bb0fb0b89377abc45c
Yuya S <y303sushi@gmail.com>
2024-11-06 19:10:51 +0900

    fix: package dep

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        port: "",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
