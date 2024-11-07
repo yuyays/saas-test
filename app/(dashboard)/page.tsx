556a496fb42cf277870cc63fae3108d845980c04
Yuya S <y303sushi@gmail.com>
2024-11-07 15:13:46 +0900

    chore: remove unused code

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,34 +1,9 @@
-"use client";
-
-import { useState } from "react";
 import LandingPage from "./landing";
-import { IKImage, IKUpload } from "imagekitio-next";
 
 export default function HomePage() {
-  const [filePath, setfilePath] = useState("");
-  const onError = (err: any) => {
-    console.log("Error", err);
-  };
-
-  const onSuccess = (res: any) => {
-    console.log("Success", res);
-    setfilePath(res.filePath);
-  };
-
   return (
     <main>
       <LandingPage />
-      {filePath && (
-        <IKImage path={filePath} width={300} height={300} alt="Alt text" />
-      )}
-      <div>
-        <h2>File upload</h2>
-        <IKUpload
-          fileName="test-upload.png"
-          onError={onError}
-          onSuccess={onSuccess}
-        />
-      </div>
     </main>
   );
 }
