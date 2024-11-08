1c8918fc3dda3101a4792452eb245dafa6630d05
Yuya S <y303sushi@gmail.com>
2024-11-08 14:11:15 +0900

    fix: create separate query file as RSC

--- app/(galleries)/galleries/page.tsx
+++ app/(galleries)/galleries/page.tsx
@@ -1,39 +1,7 @@
 import MediaGallery from "./ImageGallery";
-import { MediaFile } from "./ImageGallery";
-import imageKit from "@/lib/iamgeKit";
 import { redirect } from "next/navigation";
 import { getSession } from "@/lib/auth/session";
-import { userMedia } from "@/lib/db/schema";
-import { eq } from "drizzle-orm";
-import { db } from "@/lib/db/drizzle";
-
-async function fetchMedia(userId: number): Promise<MediaFile[]> {
-  try {
-    const userMediaItems = await db
-      .select()
-      .from(userMedia)
-      .where(eq(userMedia.userId, userId));
-
-    const mediaPromises = userMediaItems.map(async (item) => {
-      const fileDetails = await imageKit.getFileDetails(item.fileId);
-      return {
-        fileId: fileDetails.fileId,
-        name: fileDetails.name,
-        url: fileDetails.url,
-        fileType: fileDetails.fileType,
-        height: fileDetails.height,
-        width: fileDetails.width,
-        // audioCodec: fileDetails.audioCodec,
-        // videoCodec: fileDetails.videoCodec,
-      };
-    });
-
-    return await Promise.all(mediaPromises);
-  } catch (error) {
-    console.error("Failed to fetch media:", error);
-    return [];
-  }
-}
+import { fetchMedia } from "./queries";
 
 export default async function GalleryPage() {
   const session = await getSession();
@@ -45,12 +13,7 @@ export default async function GalleryPage() {
   }
   const mediaItems = await fetchMedia(session.user.id);
   if (mediaItems.length === 0) {
-    return (
-      <div>
-        please singin/login. No media found or error occurred while fetching
-        media.
-      </div>
-    );
+    return <div>No media found or error occurred while fetching media.</div>;
   }
 
   return (
---
+++ app/(galleries)/galleries/queries.ts
@@ -0,0 +1,33 @@
+import { userMedia } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
+import { db } from "@/lib/db/drizzle";
+import { MediaFile } from "./ImageGallery";
+import imageKit from "@/lib/iamgeKit";
+
+export async function fetchMedia(userId: number): Promise<MediaFile[]> {
+  try {
+    const userMediaItems = await db
+      .select()
+      .from(userMedia)
+      .where(eq(userMedia.userId, userId));
+
+    const mediaPromises = userMediaItems.map(async (item) => {
+      const fileDetails = await imageKit.getFileDetails(item.fileId);
+      return {
+        fileId: fileDetails.fileId,
+        name: fileDetails.name,
+        url: fileDetails.url,
+        fileType: fileDetails.fileType,
+        height: fileDetails.height,
+        width: fileDetails.width,
+        // audioCodec: fileDetails.audioCodec,
+        // videoCodec: fileDetails.videoCodec,
+      };
+    });
+
+    return await Promise.all(mediaPromises);
+  } catch (error) {
+    console.error("Failed to fetch media:", error);
+    return [];
+  }
+}
